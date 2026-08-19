85ad27ec1bd4f80550242371ab3d84930ac37214
Hibandd122 <hibandd122@users.noreply.github.com>
2026-08-19 20:02:37 +0700

    fix(vercel): move client scripts to js/ to prevent Vercel node function auto-detection

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'vol9-reader-v1';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './chapters.js', './manifest.webmanifest'];
+const SHELL = ['./', './index.html', './styles.css', './js/app.js', './js/chapters.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
